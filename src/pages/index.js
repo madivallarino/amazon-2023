72fe6acfdde37b78444f9994eb842e9749966a10
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2023-01-18 13:37:11 -0500

    all working

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,18 +1,37 @@
 import Head from 'next/head'
 import Image from 'next/image'
-import { Inter } from '@next/font/google'
+import Header from '@/components/Header'
+import Banner from '@/components/ Banner'
+import ProductFeed from '@/components/ProductFeed'
 
-
-const inter = Inter({ subsets: ['latin'] })
-
-export default function Home() {
+export default function Home({products }) {
+  
   return (
-    <>
+    <div className='bg-gray-100'>
       <Head>
         <title>Amazon 2023</title>
       </Head>
 
-      <h1>Hello!!! Good luck!</h1>
-    </>
+      <Header />
+
+      <main className='max-w-screen-2xl mx-auto'>
+        {/* Banner */}
+        <Banner />
+        
+        <ProductFeed products={products}/>
+      </main>
+    </div>
   )
 }
+
+
+export async function getServerSideProps(context){
+  const products = await fetch('https://fakestoreapi.com/products').then((res)=> res.json());
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
+
