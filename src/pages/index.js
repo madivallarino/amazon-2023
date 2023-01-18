e80ef0aeb67d4edccfb7ea24b612ca0241f89a6b
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2023-01-18 11:28:55 -0500

    first push

---
+++ src/pages/index.js
@@ -0,0 +1,18 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Inter } from '@next/font/google'
+
+
+const inter = Inter({ subsets: ['latin'] })
+
+export default function Home() {
+  return (
+    <>
+      <Head>
+        <title>Amazon 2023</title>
+      </Head>
+
+      <h1>Hello!!! Good luck!</h1>
+    </>
+  )
+}
