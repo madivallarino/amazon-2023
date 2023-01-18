89f9bd04d9efabed10967254e3e9172b34ebd019
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2023-01-18 18:49:54 -0500

    major changes i repeat major changes

--- src/components/Product.js
+++ src/components/Product.js
@@ -46,7 +46,7 @@ function Product({id, title, price, description, category, image}) {
         <h4 className='my-3'>{title}</h4>
         <div className='flex'>
         {Array(rating).fill().map((_, i)=> (
-            <StarIcon className='h-5 text-yellow-500' />
+            <StarIcon className='h-5 text-yellow-500' key={i}/>
         ))}
          
         </div>
--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -19,6 +19,7 @@ function Checkout() {
             <div className='flex-grow m-5 shadow-sm '>
                 <Image 
                     src="https://links.papareact.com/ikj"
+                    alt=""
                     width={1020}
                     height={250}
                 />
