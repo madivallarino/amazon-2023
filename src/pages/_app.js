c7ee969bd65d4e7706ad2897f774207ed35ac297
Madi Vallarino <85126577+madivallarino@users.noreply.github.com>
2023-01-18 18:54:29 -0500

    did i fix anything

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,6 @@
 import '@/styles/globals.css'
 import { SessionProvider  } from 'next-auth/react'
-import { store } from '../app/store'
+import { store } from '../application/store'
 import { Provider } from 'react-redux'
 export default function App({ Component, pageProps }) {
   return (
